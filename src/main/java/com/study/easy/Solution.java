2146bc3b7019a69ec2b0adb558f3f1be71d1db54
北辰丶羽 <1332112062@qq.com>
2019-07-12 21:31:51 +0800

    两数之和题目

---
+++ src/main/java/com/study/easy/Solution.java
@@ -0,0 +1,35 @@
+package com.study.easy;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 题目:
+ * 给定一个整数数组 nums 和一个目标值 target，
+ * 请你在该数组中找出和为目标值的那 两个 整数，
+ * 并返回他们的数组下标。
+ * @author guanyushen
+ * @date 2019/7/12 20:09
+ */
+
+public class Solution {
+
+    public static int[] twoSum(int[] nums, int target) throws IllegalAccessException {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{map.get(target-nums[i]),i};
+            }
+            map.put(nums[i],i );
+        }
+        throw new IllegalAccessException("找不到这样的组合!");
+    }
+
+    public static void main(String[] args) throws IllegalAccessException {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] temp = twoSum(nums,target);
+        System.out.println(Arrays.toString(temp));
+    }
+}
---
+++ src/main/java/com/study/easy/test.java
@@ -0,0 +1,9 @@
+package com.study.easy;
+
+/**
+ * @author guanyushen
+ * @date 2019/7/12 20:09
+ */
+
+public class test {
+}
