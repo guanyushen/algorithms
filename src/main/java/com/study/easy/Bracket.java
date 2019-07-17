8c9a0062d0c484578041d12ec6e672ad913e9533
北辰丶羽 <1332112062@qq.com>
2019-07-17 20:35:49 +0800

    有效的括号

---
+++ src/main/java/com/study/easy/Bracket.java
@@ -0,0 +1,49 @@
+package com.study.easy;
+
+import java.util.Stack;
+
+/**
+ * @author guanyushen
+ * @date 2019/7/17 20:14
+ */
+
+public class Bracket {
+
+    public static boolean isValid(String s) {
+
+        Stack<String> stack = new Stack<>();
+        for (int i=0; i<s.length();i++){
+            switch (s.charAt(i)){
+                case '(':
+                    stack.push("(");
+                    break;
+                case ')':
+                    if(stack.isEmpty()) return false;
+                    if(!stack.pop().equals("(")) return false;
+                    break;
+                case '[':
+                    stack.push("[");
+                    break;
+                case ']':
+                    if(stack.isEmpty()) return false;
+                    if(!stack.pop().equals("[")) return false;
+                    break;
+                case '{':
+                    stack.push("{");
+                    break;
+                case '}':
+                    if(stack.isEmpty()) return false;
+                    if(!stack.pop().equals("{")) return false;
+                    break;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "()[]{}";
+        System.out.println("输入:"+s);
+        System.out.println("结果:"+isValid(s));
+    }
+}
