b71d0121f894f75d7f6ea564c7c092a7e5a86723
北辰丶羽 <1332112062@qq.com>
2019-07-21 11:18:02 +0800

    移除元素

---
+++ src/main/java/com/study/easy/RemoveNum.java
@@ -0,0 +1,34 @@
+package com.study.easy;
+
+import java.util.Arrays;
+
+/**
+ * 题目:移除元素
+ * 给定一个数组 nums 和一个值 val，你需要原地移除所有数值等于 val 的元素，返回移除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ * @author guanyushen
+ * @date 2019/7/21 11:12
+ */
+
+public class RemoveNum {
+
+    public static int removeElement(int[] nums, int val) {
+
+        int i = 0;
+        for (int j=0 ;j<nums.length;j++){
+            if(nums[j]!=val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        System.out.println(Arrays.toString(nums));
+        System.out.println(removeElement(nums, val));
+    }
+}
