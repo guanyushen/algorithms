5478350ad8f3587250c87518883b7d25a7bcdfaf
北辰丶羽 <1332112062@qq.com>
2019-07-22 21:59:56 +0800

    最后一个字符串的长度

--- src/main/java/com/study/easy/LastString.java
+++ src/main/java/com/study/easy/LastString.java
@@ -11,17 +11,16 @@ package com.study.easy;
 public class LastString {
 
     public static int lengthOfLastWord(String s) {
-        int end = s.length()-1;
-        while(end >0 && s.charAt(end) == ' ') end--;
-        if(end == 0) return 0;
-        int start = end;
-        while (start >0 && s.charAt(start) != ' ') start--;
-        return end - start;
+        s = s.trim();
+        int start = s.lastIndexOf(" ");
+        if(start == -1) return s.length();
+        String temp = s.substring(start, s.length()-1);
+        return temp.length();
     }
 
     public static void main(String[] args) {
 
-        String s = "Hello World";
+        String s = "hello world   ";
         System.out.println(lengthOfLastWord(s));
     }
 }
