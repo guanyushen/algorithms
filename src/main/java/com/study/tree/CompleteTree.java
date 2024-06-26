ca8e65f1b0cb2e56e84aea74921907c595d7c534
guanyushen <wb_guanyushen@meituan.com>
2024-06-26 11:43:38 +0800

    feat(*): 判断是否为完全二叉树

--- src/main/java/com/study/tree/CompleteTree.java
+++ src/main/java/com/study/tree/CompleteTree.java
@@ -1,5 +1,10 @@
 package com.study.tree;
 
+import com.study.base.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @desc: 判断是否为完全二叉树
  * @author: guanyushen
@@ -7,4 +12,35 @@ package com.study.tree;
  */
 public class CompleteTree {
 
+    public static boolean isCompleteTree(TreeNode treeNode) {
+        if (treeNode == null || (treeNode.getLeft() == null && treeNode.getRight() == null)) {
+            return true;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(treeNode);
+        boolean isLastLevel = false;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if(isLastLevel && node != null) {
+                return false;
+            }
+            if (node == null) {
+                isLastLevel = true;
+                continue;
+            }
+            queue.offer(node.getLeft());
+            queue.offer(node.getRight());
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode treeNode = new TreeNode(1);
+        treeNode.setLeft(new TreeNode(2));
+        treeNode.setRight(new TreeNode(3));
+        treeNode.getLeft().setRight(new TreeNode(4));
+//        treeNode.getLeft().setLeft(new TreeNode(4));
+        System.out.println(isCompleteTree(treeNode));
+    }
 }
